9891c270a974e6f35d85bdac51aaa08f0c6f09c6
Mohamad Adil <adilansari8727@gmail.com>
2025-05-28 18:09:57 +0530

    '28-05-2025'

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  const swiper = new Swiper('.testimonial-slider', {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    loop: true,
+    centeredSlides: true,
+    grabCursor: true,
+    speed: 800,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+    },
+  });
+});
